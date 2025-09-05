952ea65bf39485cb324e20a6e8f789a0b077b078
Kumarbanoth07 <kumarbanoth87@gmail.com>
2025-09-05 22:59:37 +0530

    Conditional Statements

---
+++ Python_Projects/Conditional_or_Decision_Making_Statements.py
@@ -0,0 +1,17 @@
+#runs the code if condition is true 
+#syntax
+
+#if condition:
+
+age = 21
+if age >= 18:
+   print("you can vote")
+
+#if and else condition
+
+age = 17
+if age >=18:
+    print("you can vote")
+
+else:
+    print("you can not vote")        
